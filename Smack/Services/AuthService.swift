92f114eb2e4770c3194c74f7d5111eae3fd1e4a4
Abhishek Goyal <abhishek.goyal9419@gmail.com>
2017-12-28 20:55:28 +0530

    AuthService Added

---
+++ Smack/Services/AuthService.swift
@@ -0,0 +1,68 @@
+//
+//  AuthService.swift
+//  Smack
+//
+//  Created by Abhishek Goyal on 28/12/17.
+//  Copyright © 2017 Abhishek Goyal. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService{
+    
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn: Bool{
+        get{
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set{
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String{
+        get{
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String{
+        get{
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set{
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String,pass: String,completion: @escaping CompletionHandler){
+        
+        let lowerCaseEmail = email.lowercased()
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": pass
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil{
+                completion(true)
+            }else{
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
+    
+}
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
+
+//urls
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
