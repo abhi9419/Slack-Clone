55d611c29995e5884b4da937150a353916fbf407
Abhishek Goyal <abhishek.goyal9419@gmail.com>
2017-12-29 23:07:24 +0530

    avatar cell added

--- Smack/Controller/AvatarPickerVC.swift
+++ Smack/Controller/AvatarPickerVC.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class AvatarPickerVC: UIViewController {
+class AvatarPickerVC: UIViewController,UICollectionViewDelegate,
+UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+   
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +29,35 @@ class AvatarPickerVC: UIViewController {
     }
     
 
-  
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell{
+            
+            return cell
+            
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    
+    @IBAction func backPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+        
+        
+        
+    }
+    
 
 }
